9e9f818a61037a4394e7111c38acb79fd579e605
Sasa <sasa.zelic@outlook.com>
2023-12-03 01:37:54 -0800

    Create module.psm1

---
+++ module.psm1
@@ -0,0 +1,21 @@
+﻿function testingnew ($store){
+
+
+write-host $store -ForegroundColor Red
+
+}
+
+function novascripta ($store){
+
+
+write-host $store -ForegroundColor Red
+
+}
+
+
+function whatisthisnow ($store){
+
+
+write-host $store -ForegroundColor Red
+
+}
